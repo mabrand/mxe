0fa8821dc712a4567bf2a7439fd44c42d4db7ad6
Mark Brand <mabrand@mabrand.nl>
2025-06-04 00:58:48 +0200

    qt6: rollback to version 6.8.3

--- src/qt/qt6/qt6-qtwebview.mk
+++ src/qt/qt6/qt6-qtwebview.mk
@@ -5,7 +5,7 @@ include src/qt/qt6/qt6-conf.mk
 PKG := qt6-qtwebview
 $(eval $(QT6_METADATA))
 
-$(PKG)_CHECKSUM := c29ad4fbb8f7a2042c4941bef88bb13bd04757eed94d6937ae69e0e4ea0d59a5
+$(PKG)_CHECKSUM := 2c46e472034c8a254bb4dbd15250a3fa8f727135a14718a3ff1175697b487ed2
 $(PKG)_DEPS     := cc qt6-conf qt6-qtbase qt6-qtdeclarative
 
 QT6_PREFIX   = '$(PREFIX)/$(TARGET)/$(MXE_QT6_ID)'
