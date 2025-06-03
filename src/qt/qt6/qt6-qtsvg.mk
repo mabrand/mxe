182c18869f04c977c035a42c96574aa5a855946a
Mark Brand <mabrand@mabrand.nl>
2025-06-04 00:58:48 +0200

    qt6: rollback to version 6.8.3

--- src/qt/qt6/qt6-qtsvg.mk
+++ src/qt/qt6/qt6-qtsvg.mk
@@ -5,7 +5,7 @@ include src/qt/qt6/qt6-conf.mk
 PKG := qt6-qtsvg
 $(eval $(QT6_METADATA))
 
-$(PKG)_CHECKSUM := 2dfc5de5fd891ff2afd9861e519bf1a26e6deb729b3133f68a28ba763c9abbd5
+$(PKG)_CHECKSUM := 35eb516460f00f264eb504baa253432384351cf23fb9980a5857190e8deef438
 $(PKG)_TARGETS  := $(BUILD) $(MXE_TARGETS)
 $(PKG)_DEPS     := cc qt6-conf qt6-qtbase
 
--- src/qt/qt6/qt6-qtvirtualkeyboard.mk
+++ src/qt/qt6/qt6-qtvirtualkeyboard.mk
@@ -5,7 +5,7 @@ include src/qt/qt6/qt6-conf.mk
 PKG := qt6-qtvirtualkeyboard
 $(eval $(QT6_METADATA))
 
-$(PKG)_CHECKSUM := 80059a38bdb836f0785292396970edc108f477a68d9a35bed8393750de3d281f
+$(PKG)_CHECKSUM := 8111061261ed8d88ec40b79083f8ed025650eb1807a05528615265d36213bb1d
 $(PKG)_DEPS     := cc qt6-conf qt6-qtbase qt6-qtdeclarative qt6-qtsvg
 
 QT6_PREFIX   = '$(PREFIX)/$(TARGET)/$(MXE_QT6_ID)'
