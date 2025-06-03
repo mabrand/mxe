3db7a8deafba7d6397e96bcc5432a44da8497218
Mark Brand <mabrand@mabrand.nl>
2025-06-04 00:58:48 +0200

    qt6: rollback to version 6.8.3

--- src/qt/qt6/qt6-qtimageformats.mk
+++ src/qt/qt6/qt6-qtimageformats.mk
@@ -5,7 +5,7 @@ include src/qt/qt6/qt6-conf.mk
 PKG := qt6-qtimageformats
 $(eval $(QT6_METADATA))
 
-$(PKG)_CHECKSUM := ebe9f238daaf9bb752c7233edadf4af33fc4fa30d914936812b6410d3af1577c
+$(PKG)_CHECKSUM := 049bfb99845e4801672aca07c3c4fc4c140f932a3a33faa899419579e33ef1c8
 $(PKG)_DEPS     := cc qt6-conf qt6-qtbase jasper libmng libwebp tiff
 
 QT6_PREFIX   = '$(PREFIX)/$(TARGET)/$(MXE_QT6_ID)'
